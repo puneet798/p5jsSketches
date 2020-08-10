00a58f1fa120a4e2edeacaf18c70b52eb891840f
puneet798 <puneet798@gmail.com>
2020-08-10 14:48:17 +0530

    FloatingTorus_Sketch

---
+++ FloatingTorus/sketch.js
@@ -0,0 +1,16 @@
+var angle=0;
+
+function setup() {
+  createCanvas(600, 600, WEBGL);
+}
+
+function draw() {
+  background(250,140,190);
+  rectMode(CENTER);
+  rotateX(angle);
+  rotateZ(angle*0.15);
+  fill(10,20,30);
+  //box(100,34,100);
+  torus(23,100);
+  angle=angle+0.05;
+}
