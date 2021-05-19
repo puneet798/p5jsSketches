c3acd39e4c38150edfb79c90130293f15a0a6fcd
puneet798 <puneet798@gmail.com>
2021-05-19 12:19:51 +0530

    Update sketch.js

--- CreationOfBirds3/sketch.js
+++ CreationOfBirds3/sketch.js
@@ -16,9 +16,9 @@ function preload(){
 }
 
 function setup() {
-  canvas = createCanvas(800,1800);
-  leftBuffer = createGraphics(800, 900);
-  rightBuffer = createGraphics(800, 900);
+  canvas = createCanvas(800,800);
+  leftBuffer = createGraphics(800, 400);
+  rightBuffer = createGraphics(800, 400);
   textFont('helvetica', 14);
   //textFont(myFont);
   textLeading(21);
